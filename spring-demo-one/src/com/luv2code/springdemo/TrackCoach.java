8d7b20dabb8193516f557210c8c33bfc2aefdaa2
xuhuan <185062278@qq.com>
2022-11-17 22:32:26 -0500

    added first spring project

---
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+
+
+
+
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
