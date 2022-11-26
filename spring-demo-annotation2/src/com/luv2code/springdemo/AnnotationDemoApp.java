9c45667316cdd39863e32bad65460bf2d80f459c
Xuhuan He <xuhuanhe16@example.com>
2022-11-25 22:16:13 -0500

    added default component names

--- spring-demo-annotation2/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotation2/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -6,7 +6,7 @@ public class AnnotationDemoApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		Coach coach = context.getBean("TennisCoach", Coach.class);
+		Coach coach = context.getBean("tennisCoach", Coach.class);
 		
 		
 		System.out.println(coach.getDailyWorkout());
--- spring-demo-annotation2/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotation2/src/com/luv2code/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("TennisCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
