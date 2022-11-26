9de0768d3a829fe47518ade1591ce3271072a492
Xuhuan He <xuhuanhe16@example.com>
2022-11-25 22:27:30 -0500

    added practice activity 4

---
+++ spring-demo-annotation2/src/com/luv2code/springdemo/Practice4.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Practice4 implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "pract22222ice4";
+	}
+
+}
---
+++ spring-demo-annotation2/src/com/luv2code/springdemo/Practice4Main.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Practice4Main {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach coach = context.getBean("practice4", Coach.class);
+		
+		
+		System.out.println(coach.getDailyWorkout());
+	}
+}
