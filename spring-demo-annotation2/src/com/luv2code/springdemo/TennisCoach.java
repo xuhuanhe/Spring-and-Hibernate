17322a6d0416ad82dec2364b1b4f65edf94b510e
Xuhuan He <xuhuanhe16@example.com>
2022-11-26 11:31:57 -0500

    added dependency injection - setter injection

--- spring-demo-annotation2/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotation2/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,13 +1,31 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	private Fortuneservice f;
+//	@Autowired
+//	public TennisCoach(Fortuneservice f) {
+//		this.f = f;
+//	}
+	@Autowired
+	public void setF(Fortuneservice f) {
+		this.f = f;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "TennisCoa654654ch";
+	}
+
+	
+
+	@Override
+	public String getDailyFortune() {
+		return f.getFortune();
 	}
 
 }
