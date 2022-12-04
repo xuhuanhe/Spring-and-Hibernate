6c1dc95ecc06dcf649ddee6685e2bdb92cb80ca3
Xuhuan He <xuhuanhe16@example.com>
2022-12-04 17:17:41 -0500

    added spring mvc project

---
+++ spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h2>Spring MC Demo - Home Page</h2>
+
+<hr>
+
+<a href="showForm">Hello World form</a>
+</body>
+</html>
---
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/showForm") 
+	public String showForm(){ 
+		return "helloworld-form"; 
+	}
+	
+	@RequestMapping("/processForm") 
+	public String processForm(){ 
+		return "helloworld"; 
+	}
+	
+
+}
