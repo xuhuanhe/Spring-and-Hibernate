15ce4a2b903674234fe572478dbeee8ca8368e64
Xuhuan He <xuhuanhe16@example.com>
2022-12-04 17:40:53 -0500

    added "adding data to the spring model" pages

--- spring-mvc-demo/WebContent/WEB-INF/view/helloworld.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/helloworld.jsp
@@ -8,7 +8,9 @@ Hello World of Spring!
 <br><br>
 
 Student name: ${param.studentName}
+<br><br>
 
+The message: ${message}
 </body>
 
 </html>
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -19,5 +19,29 @@ public class HelloWorldController {
 		return "helloworld"; 
 	}
 	
+	@RequestMapping("/processFormVersionTwo")  
+	public String letsShoutDude(HttpServletRequest request, Model model){  
+
+		//read the request parameter from the html form 
+	
+		String theName = request.getParameter("studentName"); 
+	
+		//convert the data to all caps 
+	
+		theName = theName.toUpperCase(); 
+	
+		//create the message 
+	
+		String result = "Yo!" + theName; 
+	
+		//add message to the model 
+	
+		model.addAttribute("message",result); 
+	
+		return "helloworld";  
+
+	} 
+
+	 
 
 }
