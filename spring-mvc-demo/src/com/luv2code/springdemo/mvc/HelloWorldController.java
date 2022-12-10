7f3dc943be80beef2de33039e0b5b1df3a00aeff
Xuhuan He <xuhuanhe16@example.com>
2022-12-10 16:25:32 -0500

    added spring MVC form tag - text field

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 	
 	@RequestMapping("/showForm") 
