e6e885c99c0ecc4e4e080bdb421780c94f113278
Xuhuan He <xuhuanhe16@gmail.com>
2023-01-14 20:00:28 -0500

    added one to one mapping main

--- hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -12,31 +12,48 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="instructor")
-
-
 public class Instructor {
+
+	// annotate the class as an entity and map to db table
+	
+	// define the fields
+	
+	// annotate the fields with db column names
+	
+	// ** set up mapping to InstructorDetail entity
+	
+	// create constructors
+	
+	// generate getter/setter methods
+	
+	// generate toString() method
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
+	
 	@Column(name="first_name")
 	private String firstName;
+	
 	@Column(name="last_name")
 	private String lastName;
+
 	@Column(name="email")
 	private String email;
 	
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
+	
+	public Instructor() {
+		
+	}
 
-	
-	
-	
-	@Override
-	public String toString() {
-		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", instructorDetail=" + instructorDetail + "]";
+	public Instructor(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
 	}
 
 	public int getId() {
@@ -79,16 +96,17 @@ public class Instructor {
 		this.instructorDetail = instructorDetail;
 	}
 
-	public Instructor() {
-		
-	}
-	
-	public Instructor(String firstName, String lastName, String email) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", instructorDetail=" + instructorDetail + "]";
 	}
 	
 	
 }
+
+
+
+
+
+
