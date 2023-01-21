1271547119675324f6e0f71d1201c32793d64fd6
Xuhuan He <xuhuanhe16@gmail.com>
2023-01-21 00:28:13 -0500

    added delete and get instructorDetail

--- hb-02-one-to-one-bi/src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
+++ hb-02-one-to-one-bi/src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -8,7 +8,7 @@ import org.hibernate.cfg.Configuration;
 import com.luv2code.hibernate.demo.entity.Instructor;
 import com.luv2code.hibernate.demo.entity.InstructorDetail;
 
-public class GetInstructorDetailDemo {
+public class DeleteInstructorDetailDemo {
 
 	public static void main(String[] args) {
 
@@ -36,6 +36,8 @@ public class GetInstructorDetailDemo {
 			System.out.println(temp);
 			//print the associated instructor
 			System.out.println(temp.getInstructor());
+			//now let's delte the instructor detail
+			
 			// commit transaction
 			session.getTransaction().commit();
 			
