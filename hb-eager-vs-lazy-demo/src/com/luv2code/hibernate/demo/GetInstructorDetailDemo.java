27aee07e85d5e45adb36c2954a1d70ae19b40858
Xuhuan He <xuhuanhe16@gmail.com>
2023-01-25 21:52:47 -0500

    added hb-eager-vs-lazy-demo project

---
+++ hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,57 @@
+package com.luv2code.hibernate.demo;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
+			session.beginTransaction();
+
+			
+			//get the instructor detail object
+			int theId = 9;
+			InstructorDetail temp = session.get(InstructorDetail.class, theId);
+			
+			//print the instructor detail
+			System.out.println(temp);
+			//print the associated instructor
+			System.out.println(temp.getInstructor());
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}catch(Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
