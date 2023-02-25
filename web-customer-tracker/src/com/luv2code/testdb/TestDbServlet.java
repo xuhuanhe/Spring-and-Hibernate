ac6ee36bd0299904e25a61f6518ce1e21bbf30b0
Xuhuan He <xuhuanhe16@gmail.com>
2023-02-25 17:44:05 -0500

    modified list-customers.jsp

--- web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
+++ web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
@@ -2,14 +2,14 @@ package com.luv2code.testdb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import java.sql.*;
 
 /**
  * Servlet implementation class TestDbServlet
@@ -21,36 +21,37 @@ public class TestDbServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		// setup connection variables
 		String user = "springstudent";
 		String pass = "springstudent";
-		
+
 		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 		String driver = "com.mysql.cj.jdbc.Driver";
-		
+
 		// get connection to database
 		try {
 			PrintWriter out = response.getWriter();
-			
+
 			out.println("Connecting to database: " + jdbcUrl);
-			
+
 			Class.forName(driver);
-			
+
 			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
-			
+
 			out.println("SUCCESS!!!");
-			
+
 			myConn.close();
-			
+
 		}
 		catch (Exception exc) {
 			exc.printStackTrace();
 			throw new ServletException(exc);
 		}
-	
-	
+
+
 	}
 
 }
